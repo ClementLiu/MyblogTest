026b7ee52916ae7cbcc00c1893918d3372def480
ClementLiu <ziwei.liu@cca.edu>
2017-07-09 13:08:32 +0800

    Revert "Log"
    This reverts commit add8556eefd71cbddcf2cd709bc47737dc93578a.

--- index.js
+++ index.js
@@ -52,32 +52,18 @@ app.use(function (req, res, next) {
   next();
 });
 
-// 正常请求的日志
-app.use(expressWinston.logger({
-  transports: [
-    new (winston.transports.Console)({
-      json: true,
-      colorize: true
-    }),
-    new winston.transports.File({
-      filename: 'logs/success.log'
-    })
-  ]
-}));
 // 路由
 routes(app);
-// 错误请求的日志
-app.use(expressWinston.errorLogger({
-  transports: [
-    new winston.transports.Console({
-      json: true,
-      colorize: true
-    }),
-    new winston.transports.File({
-      filename: 'logs/error.log'
-    })
-  ]
-}));
+<<<<<<< HEAD
+=======
+
+// error page
+app.use(function (err, req, res, next) {
+  res.render('error', {
+    error: err
+  });
+});
+>>>>>>> parent of add8556... Log
 
 // 监听端口，启动程序
 app.listen(config.port, function () {
